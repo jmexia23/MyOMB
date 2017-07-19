8e16c6debfa14ff6e2c84e5cca989d8400c6a903
João Mexia <Joo Mexia>
2017-07-19 15:51:34 +0100

    Add project files.

---
+++ Test/Test/LogIn.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LogIn : ContentPage
+	{
+		public LogIn ()
+		{
+			InitializeComponent ();
+		}
+
+        private void OnSignUpClicked(Object sender , EventArgs e)
+        {
+            DisplayAlert("Sign Up", "Sign Up Efectuado com sucesso", "Continuar");        }
+
+        private void OnLoginClicked(Object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new Index());
+        }
+
+
+    }
+}
